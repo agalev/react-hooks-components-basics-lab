d111b2c7272f82e811ca3e99825f8e64d2a997ab
Alexander <alex.galev@yahoo.com>
2023-02-21 14:47:15 -0700

    changed filename

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from './Navbar'
+import NavBar from './NavBar'
 import Home from './Home'
 import About from './About'
 
 function App() {
 	return (
 		<div>
-			<Navbar />
+			<NavBar />
 			<Home />
 			<About />
 		</div>
